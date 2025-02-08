07c33e4822ef2e0a31cd8e6b1862edf255e72c8e
Satyatej10 <satyatejinturi@gmail.com>
2025-02-08 18:13:11 +0530

    backend

--- server/routes/NameRoute.js
+++ server/routes/NameRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getRestaurantByName } = require("../controllers/NameController"); // Import controller
 
 // Define route for searching restaurants
-router.get("/", getRestaurantByName);
+router.get("/searchbyname/:name", getRestaurantByName);
 
 module.exports = router;
